8db14c8972d88335b1e1d54922976a266a68f2d8
Ben Hong <ben@bencodezen.io>
2021-02-19 16:27:27 -0500

    add options check

--- functions/cors-lab.js
+++ functions/cors-lab.js
@@ -1,17 +1,26 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async event => {
   console.log('--------', event)
+  if (event.httpMethod === 'OPTIONS') {
+    const netlifyDomainRegex = /netlify.app$/
+
+    if (netlifyDomainRegex.test(event.headers.origin)) {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': event.headers.origin,
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
+          'Access-Control-Allow-Headers':
+            'Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-test'
+        }
+      }
+    }
+  }
 
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers':
-          'Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-test'
-      },
       body: JSON.stringify({
         message: `Hello World`
       })
